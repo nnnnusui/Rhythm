fb1c90465c4d61ce4019ef149758c7f4d538f586
nnnnusui <nnnnusui@gmail.com>
2021-06-02 06:43:10 +0900

    add: suppressTouchMove

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,13 @@
 import { Player } from "./player/Player";
 import { score } from "./sample-score";
 
+const suppressTouchMove = () => {
+  window.addEventListener("touchmove", (e: TouchEvent) => e.preventDefault(), {
+    passive: false,
+  });
+};
+suppressTouchMove();
+
 const player = Player({ score });
 
 const root = document.body;
