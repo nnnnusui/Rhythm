7cc53fa3ec7a698e8c4106bd68543ca6d1c119a2
nnnnusui <nnnnusui@gmail.com>
2021-06-04 06:06:55 +0900

    add: enableContinuousTouch

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,8 +1,11 @@
 import { Game } from "./Game";
 import { score } from "./sample-score";
 
+const enableContinuousTouch = () => {
+  document.addEventListener("touchend", (e) => e.preventDefault());
+};
 const suppressTouchMove = () => {
-  window.addEventListener("touchmove", (e: TouchEvent) => e.preventDefault(), {
+  document.addEventListener("touchmove", (e) => e.preventDefault(), {
     passive: false,
   });
 };
@@ -18,6 +21,7 @@ starter.addEventListener(
   () => {
     starter.remove();
     suppressTouchMove();
+    enableContinuousTouch();
     const game = Game({ score });
     root.append(game.element);
   },
