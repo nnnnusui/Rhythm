387fb2bc92c8ba8e07b41f768d0755a2c9bec001
nnnnusui <nnnnusui@gmail.com>
2021-06-15 12:13:21 +0900

    add: game-select-menu

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,18 +1,38 @@
+import { scoreList } from "./sample-score";
 import { Game } from "./Game";
-import { score } from "./sample-score";
+import { Score } from "./score/Score";
 
 const root = document.body;
 const starter = document.createElement("div");
-starter.textContent = "Click to start";
 starter.classList.add("starter");
+starter.textContent = "Click to start";
 root.append(starter);
 
-starter.addEventListener(
-  "click",
-  () => {
-    starter.remove();
-    const game = Game({ score });
-    root.append(game.element);
-  },
-  { once: true }
-);
+starter.onclick = () => {
+  starter.remove();
+  const GameSelectView = () => {
+    const element = document.createElement("div");
+    element.classList.add("game-select-menu");
+    const gameSelectors = scoreList.map((url) => {
+      const element = document.createElement("div");
+      element.classList.add("game-selector");
+      element.textContent = url;
+      element.addEventListener("pointerup", () => {
+        root.getElementsByClassName("game")[0]?.remove();
+        fetch(url)
+          .then((it) => it.json())
+          .then((it) => {
+            const score = Score.build(it);
+            const game = Game({ score });
+            game.element.dataset.gameId = url;
+            root.prepend(game.element);
+          });
+      });
+      return element;
+    });
+    element.append(...gameSelectors);
+    return { element };
+  };
+  const gameSelectView = GameSelectView();
+  root.append(gameSelectView.element);
+};
