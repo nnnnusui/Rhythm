2180f3787630c1329a790e9af04cbaa2e24be13c
nnnnusui <nnnnusui@gmail.com>
2021-06-15 12:29:56 +0900

    add: duration variable

--- src/main/Game.ts
+++ src/main/Game.ts
@@ -93,7 +93,7 @@ const Game = (args: { score: Score }) => {
     parameters: [
       NumberInputter(
         "offset",
-        (value) => element.style.setProperty("--offset", `${value}`),
+        (value) => element.style.setProperty("--offset", `${value * 1000}`),
         { value: 0, step: 0.01, max: 2, min: -2 }
       ),
     ],
