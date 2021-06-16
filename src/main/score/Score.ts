d5cd43526724757439dba44e7a5a3de92bd6ef9d
nnnnusui <nnnnusui@gmail.com>
2021-06-16 16:39:20 +0900

    add: offset to the Score.build() argument

--- src/main/score/Score.ts
+++ src/main/score/Score.ts
@@ -17,11 +17,12 @@ const Score = {
     args: Omit<Score, "notes" | "length"> &
       Order & {
         bpm: number;
+        offset?: number;
       }
   ): Score => {
     const notes = Context.generateNotes({
       order: args,
-      offset: 0,
+      offset: args.offset ? args.offset * 1000 : 0,
       division: 1,
       base: 60 * 1000,
       bpm: args.bpm,
