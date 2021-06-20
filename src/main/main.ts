991de7bb2fe9e965de76ac2818f90706985f3879
nnnnusui <nnnnusui@gmail.com>
2021-06-20 12:05:38 +0900

    update: append source before starting

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,29 +3,32 @@ import { Score } from "./score/Score";
 import { Source } from "./source/Source";
 
 const root = document.body;
-const starter = document.createElement("div");
-starter.classList.add("starter");
-starter.textContent = "Click to start";
-root.append(starter);
 
 const scorePath = new URLSearchParams(
   document.location.search.substring(1)
 ).get("scorePath");
-starter.onclick = () => {
-  starter.remove();
-  fetch(scorePath ? scorePath : "")
-    .then((it) => it.json())
-    .then((it) => {
-      const score = Score.build(it);
-      const source = Source.from({
-        ...score.source,
-        size: {
-          width: document.body.clientWidth,
-          height: document.body.clientHeight,
-        },
-        onReady: () => {},
-      });
-      const game = Game({ source, score });
-      root.append(source.element, game.element);
+
+fetch(scorePath ? scorePath : "")
+  .then((it) => it.json())
+  .then((it) => {
+    const score = Score.build(it);
+    const source = Source.from({
+      ...score.source,
+      size: {
+        width: document.body.clientWidth,
+        height: document.body.clientHeight,
+      },
+      onReady: () => {},
     });
-};
+
+    const starter = document.createElement("div");
+    starter.classList.add("starter");
+    starter.textContent = "Click to start";
+    root.append(source.element, starter);
+
+    starter.onclick = () => {
+      starter.remove();
+      const game = Game({ source, score });
+      root.append(game.element);
+    };
+  });
