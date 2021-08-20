7e4158b9847f8067225fa467603cf0a7ffe81d13
nnnnusui <nnnnusui@gmail.com>
2021-08-20 09:52:08 +0900

    add: YouTube.volume(value?)

--- src/main/source/Source.ts
+++ src/main/source/Source.ts
@@ -1,22 +1,35 @@
-import youTubePlayerFactory from "youtube-player";
+import { Property } from "../util/Property";
 
 let tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.prepend(tag);
-declare const YT: any;
+type YT = any;
+declare const YT: YT;
 
 // https://developers.google.com/youtube/iframe_api_reference
 const YouTube = (args: { width: number; height: number; videoId: string }) => {
   const element = document.createElement("div");
+  const onPlayerReady = ({ target }: { target: YT }) => {
+    target.setVolume(0);
+  };
   let player = new YT.Player(element, {
     ...args,
     events: {
-      // onReady: onPlayerReady,
+      onReady: onPlayerReady,
       // onStateChange: onPlayerStateChange,
     },
   });
   return {
     element: player.h,
+    volume: Property.new<number>({
+      init: 0,
+      observers: [
+        ({ next }) => {
+          console.log(next);
+          player.setVolume(next);
+        },
+      ],
+    }).accessor,
   };
 };
 
