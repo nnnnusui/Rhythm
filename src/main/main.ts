13d75547c29ee01b7ab9250a522d4beef3cac80f
nnnnusui <nnnnusui@gmail.com>
2021-08-20 18:01:29 +0900

    add: rollContainer

--- src/main/main.ts
+++ src/main/main.ts
@@ -26,8 +26,32 @@ window.addEventListener("load", async () => {
     const source = Source.fromUrlString(sourcePath);
     if (source === "Not supported.") return;
     const target = document.createElement("div");
+    target.classList.add("source");
     document.body.append(target);
     return await source({ target });
   })();
+
+  const rollContainer = (() => {
+    const element = document.createElement("section");
+    element.classList.add("roll-container");
+    const header = document.createElement("h1");
+    header.textContent = "RollContainer";
+    element.append(header);
+
+    return { element };
+  })();
+
+  const scoreMaker = (() => {
+    const element = document.createElement("section");
+    element.classList.add("score-maker");
+    const header = document.createElement("h1");
+    header.textContent = "ScoreMaker";
+    element.append(header, rollContainer.element);
+    document.body.append(element);
+    rollContainer.element.style.height = `${source.duration * 50}%`;
+    element.scrollTop = element.scrollHeight;
+    return { element };
+  })();
+
   console.log(source);
 });
