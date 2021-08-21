5ed0c3d7ffb0beac4a49968ee2dc3b6043090a2e
nnnnusui <nnnnusui@gmail.com>
2021-08-21 23:05:23 +0900

    Scroll follows playback time while playing source

--- src/main/extension/HTMLElement.extension.ts
+++ src/main/extension/HTMLElement.extension.ts
@@ -3,6 +3,7 @@ declare global {
   interface HTMLElement {
     scrollTopPercentage: number;
     scrollLeftPercentage: number;
+    scrollTopLinearly(to: number, duration: number): { cancel: () => void };
   }
 }
 
@@ -14,5 +15,26 @@ Object.defineProperty(HTMLElement.prototype, "scrollTopPercentage", {
 Object.defineProperty(HTMLElement.prototype, "scrollLeftPercentage", {
   get: function () {
     return this.scrollLeft / (this.scrollWidth - this.clientWidth);
+  },
+});
+
+Object.defineProperty(HTMLElement.prototype, "scrollTopLinearly", {
+  value: function (to: number, duration: number) {
+    const startTime = performance.now();
+    const range = to - this.scrollTop;
+    let canceled = false;
+    const loop = (nowTime) => {
+      if (canceled) return;
+      const elapsedTime = nowTime - startTime;
+      if (duration <= elapsedTime) {
+        this.scrollTo(0, to);
+        return;
+      }
+      const progress = elapsedTime / duration;
+      this.scrollTo(0, to - range * (1 - progress));
+      requestAnimationFrame(loop);
+    };
+    requestAnimationFrame(loop);
+    return { cancel: () => (canceled = true) };
   },
 });
--- src/main/main.ts
+++ src/main/main.ts
@@ -71,12 +71,20 @@ window.addEventListener("load", async () => {
     };
   })();
 
+  let scrollEventCanceller: ReturnType<HTMLElement["scrollTopLinearly"]> = {
+    cancel: () => {},
+  };
   scoreMaker.element.addEventListener("click", () => {
     switch (source.state()) {
       case "playing":
+        scrollEventCanceller.cancel();
         scoreMaker.linkingWithVideo(true);
         return source.pause();
       default:
+        scrollEventCanceller = scoreMaker.element.scrollTopLinearly(
+          0,
+          source.duration * 1000
+        );
         scoreMaker.linkingWithVideo(false);
         return source.play();
     }
