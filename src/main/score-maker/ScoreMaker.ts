7bcea42fb64ae0f95e4e8e08bbfd14bba238570f
nnnnusui <nnnnusui@gmail.com>
2021-09-02 17:26:40 +0900

    update: ScoreMaker's Time information management
    ScoreMaker.timer
    => ScoreMaker.time
    => source.time

--- src/main/score-maker/ScoreMaker.ts
+++ src/main/score-maker/ScoreMaker.ts
@@ -28,26 +28,69 @@ const ScoreMaker = {
 
     element.scrollTop = element.scrollHeight;
 
-    const applyScrollProgressToSource = () => {
-      const scrollPercentage = 1 - element.scrollTopPercentage;
-      source.time(source.duration * scrollPercentage);
-    };
-
     let scrollEventCanceller: ReturnType<HTMLElement["scrollTopLinearly"]> = {
       cancel: () => {},
     };
 
+    const applyTimeToSourceObserver = ({ next }) => {
+      source.time(next);
+    };
+    const duration = Property.new({
+      init: source.duration,
+    }).accessor;
+    const { accessor: time, observers: timeObservers } = Property.new({
+      init: source.time(),
+      observers: [applyTimeToSourceObserver],
+    });
+    const applyScrollProgressToTime = () => {
+      const scrollPercentage = 1 - element.scrollTopPercentage;
+      time(duration() * scrollPercentage);
+    };
+    const timer = (() => {
+      // TODO: carve out
+      // args: { onStart, onStop, onTimer: (time) => void }
+      let canceled = true;
+      return {
+        start: () => {
+          canceled = false;
+          const to = duration();
+          const startTime = performance.now();
+          const range = to - time();
+          const loop = (nowTime) => {
+            const elapsedTime = nowTime - startTime;
+            if (to <= elapsedTime) {
+              time(to);
+              return;
+            }
+            const progress = elapsedTime / to;
+            time(to - range * (1 - progress));
+            if (canceled) return;
+            requestAnimationFrame(loop);
+          };
+          requestAnimationFrame(loop);
+        },
+        stop: () => {
+          canceled = true;
+        },
+      };
+    })();
     type Mode = "play" | "edit" | "preview";
     const mode = (() => {
       const play = () => {
         scrollEventCanceller = element.scrollTopLinearly(0, source.duration);
-        element.removeEventListener("scroll", applyScrollProgressToSource);
+        element.removeEventListener("scroll", applyScrollProgressToTime);
         source.play();
+        timeObservers
+          .filter((it) => it === applyTimeToSourceObserver)
+          .forEach((_, index) => timeObservers.splice(index));
+        timer.start();
       };
       const pause = () => {
         scrollEventCanceller.cancel();
-        element.addEventListener("scroll", applyScrollProgressToSource);
+        element.addEventListener("scroll", applyScrollProgressToTime);
         source.pause();
+        timeObservers.push(applyTimeToSourceObserver);
+        timer.stop();
       };
       return Property.new<Mode>({
         init: "edit",
@@ -64,6 +107,7 @@ const ScoreMaker = {
       }).accessor;
     })();
     element.addEventListener("click", () => {
+      console.log(time());
       switch (source.state()) {
         case "playing":
           return mode("edit");
