30f77068ccf312a602c82f41b348152c316a351f
nnnnusui <nnnnusui@gmail.com>
2022-10-26 15:07:00 +0000

    Glow `<judgeLine/>` on judge.

--- src/component/entity/JudgeLine.tsx
+++ src/component/entity/JudgeLine.tsx
@@ -1,14 +1,34 @@
 import {
   Component,
+  createEffect,
 } from "solid-js";
 
+import { useGame } from "../../context/game";
 import styles from "./JudgeLine.module.styl";
 
 const JudgeLine: Component = () => {
+  const [game] = useGame();
+
+  let element!: HTMLDivElement;
+  createEffect(() => {
+    game.recentJudge();
+    window.requestAnimationFrame(() => {
+      element.classList.add(styles.Suppress);
+      window.requestAnimationFrame(() => {
+        element.classList.remove(styles.Suppress);
+      });
+    });
+  });
+
   return (
     <div
       class={styles.JudgeLine}
-    />
+    >
+      <div
+        ref={element}
+        class={styles.JudgeGlow}
+      />
+    </div>
   );
 };
 
