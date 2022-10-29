6efd64a21375932f0859df792f4f5e686a8b6463
nnnnusui <nnnnusui@gmail.com>
2022-10-29 12:02:16 +0000

    `<Note />` animation definitions were changed from css to js.

--- src/component/entity/Note.tsx
+++ src/component/entity/Note.tsx
@@ -1,5 +1,7 @@
 import {
   Component,
+  createEffect,
+  createSignal,
   Show,
 } from "solid-js";
 
@@ -9,23 +11,44 @@ import styles from "./Note.module.styl";
 
 const Note: Component<Type> = (props) => {
   const [game] = useGame();
-  const duration = () => game.duration();
-  const progress = () => props.progress();
+  const [ref, setRef] = createSignal<HTMLDivElement>();
+  const [animation, setAnimation] = createSignal<Animation>();
 
-  const delay = () =>
-    progress() - duration();
+  const durationMs = () => game.duration() * 1000;
+  const progressMs = () => props.progress() * 1000;
+
+  createEffect(() => {
+    const element = ref();
+    if (!element) return;
+    const fullDurationMs = durationMs() * 2;
+    const animation
+      = element.animate([
+        { top: 0 },
+        { top: "80%" },
+        { top: "160%" },
+      ], {
+        duration: fullDurationMs,
+        delay: props.time(),
+        fill: "forwards",
+      });
+    animation.pause();
+    setAnimation(animation);
+  });
+  createEffect(() => {
+    const state = animation();
+    if (state === undefined) return;
+    state.currentTime = durationMs() - progressMs();
+    console.log(state.currentTime);
+  });
 
   return (
     <Show when={!props.judgement()}>
       <div
+        ref={setRef}
         class={styles.Note}
-        style={{
-          "animation-duration": `${duration() * 2}s`,
-          "animation-delay": `${delay()}s`,
-        }}
       >
         {props.time().toFixed(1)}
-      _ {progress().toFixed(1)}
+      _ {props.progress().toFixed(1)}
       </div>
     </Show>
   );
