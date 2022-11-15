3ee29cb4514cf4678f130ada7b30f79c59ce7ec4
nnnnusui <nnnnusui@gmail.com>
2022-11-15 21:56:53 +0000

    📦️: vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import solidPlugin from "vite-plugin-solid";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
+    tsconfigPaths(),
     VitePWA({
       registerType: "autoUpdate",
       devOptions: { enabled: true },
