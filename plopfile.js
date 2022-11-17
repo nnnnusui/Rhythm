dccfdc4bf8b5da66f5c7f83de523bd3890eb0a06
nnnnusui <nnnnusui@gmail.com>
2022-11-17 18:32:34 +0000

    🛠️: `plopfile.js`

---
+++ plopfile.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { glob } = require("glob");
+const templateRoot = ".template";
+
+module.exports = (
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) => {
+};
