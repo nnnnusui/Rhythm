c0accfb088c9dcfb945c998aa08b3f4c2fac4b7e
nnnnusui <nnnnusui@gmail.com>
2023-01-29 19:04:53 +0000

    `<ResourcePlayer />` base files

---
+++ src/component/ResourcePlayer/index.tsx
@@ -0,0 +1,18 @@
+import {
+  Component,
+} from "solid-js";
+
+import styles from "./index.module.styl";
+
+type Props = unknown
+
+const This: Component<Props> = (props) => {
+
+  return (
+    <div
+      class={styles.Root}
+    />
+  );
+};
+
+export default This;
